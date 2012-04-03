09cb4901beefcbc258ff35d3c99ec20c1d355e44
Dmitry Ketov <dketov@gmail.com>
2012-04-03 12:13:49 +0400

    version update

--- version.php
+++ version.php
@@ -1,7 +1,7 @@
 <?php
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2012032900;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2012040200;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2011112900;        // Requires this Moodle version
 $plugin->component = 'block_mycourses'; // Full name of the plugin (used for diagnostics)
 
