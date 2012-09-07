a940b4a457e4211a5f6585d763afab5212b3d0c2
Dmitry Ketov <dketov@gmail.com>
2012-09-07 15:40:20 +0400

    minor fix for recommended courses out of enrolled

--- block_mycourses.php
+++ block_mycourses.php
@@ -49,63 +49,62 @@ class block_mycourses extends block_base {
                     $mycourses[$id]->courses[$course->id] = $course;
                 }
             }
-
-            if($idnumber = trim($DB->get_field('user', 'idnumber', array('id' => $USER->id))))
-            {
-                if($groups = block_mycourses_get_groups_by_name($idnumber)) {
-                    $r = new stdClass;
-                    $r->enrolledas = get_string('recommended', 'block_mycourses', $idnumber);
-
-                    foreach($groups as $group) {
-                        $coursecontext = get_context_instance(CONTEXT_COURSE, $group->courseid);
-                        if(!is_enrolled($coursecontext, $USER)) {
-                            $r->courses[] = block_mycourses_get_course_by_id($group->courseid);
-                        }
-                    }
-                    if(!empty($r->courses)) {
-                        $mycourses['recommended'] = $r;
-                    }
-                }
-            }
-
-            $uicon = html_writer::empty_tag('img', array('src' => $OUTPUT->pix_url('t/user'),
-                                                  'class' => 'smallicon'));
-            $cicon = html_writer::empty_tag('img', array('src' => $OUTPUT->pix_url('i/course'),
-                                                  'class' => 'icon'));
-
-            foreach($mycourses as $k => $r) {
-                $list = array();
-                foreach($r->courses as $course) {
-                    $link = new moodle_url('/course/view.php', array('id' => $course->id));
-
-                    if($k === 'recommended') {
-                        $coursecontext = get_context_instance(CONTEXT_COURSE, $course->id);
-                        $coursecontactroles = explode(',', $CFG->coursecontact);
-
-                        $teachers = array();
-                        foreach ($coursecontactroles as $roleid) {
-                            $users = get_role_users($roleid, $coursecontext);
-                            foreach ($users as $user) {
-                                $teachers[] = $uicon . html_writer::link(new moodle_url($CFG->wwwroot.'/message/', 
-                                                                array('id' => $user->id)), fullname($user));
-                            }
-                        }
-                        $teachers = html_writer::alist($teachers);
-                    }
-
-                    $list[] = $cicon . html_writer::link($link, format_string($course->fullname)) . $teachers;
-                }
-                $this->content->text .= html_writer::tag('div', 
-                                                         html_writer::tag('h3', $r->enrolledas) .
-                                                         html_writer::alist($list, 
-                                                                          array('class' => 'unlist')),
-                                                         array('class' => $k));
-            }
-    
-            $this->content->footer = html_writer::link(new moodle_url('/course/index.php'), 
-                                                       get_string("fulllistofcourses")) . " &hellip;";
-
         }
+
+        if($idnumber = trim($DB->get_field('user', 'idnumber', array('id' => $USER->id))))
+        {
+            if($groups = block_mycourses_get_groups_by_name($idnumber)) {
+                $r = new stdClass;
+                $r->enrolledas = get_string('recommended', 'block_mycourses', $idnumber);
+
+                foreach($groups as $group) {
+                    $coursecontext = get_context_instance(CONTEXT_COURSE, $group->courseid);
+                    if(!is_enrolled($coursecontext, $USER)) {
+                        $r->courses[] = block_mycourses_get_course_by_id($group->courseid);
+                    }
+                }
+                if(!empty($r->courses)) {
+                    $mycourses['recommended'] = $r;
+                }
+            }
+        }
+
+        $uicon = html_writer::empty_tag('img', array('src' => $OUTPUT->pix_url('t/user'),
+                                              'class' => 'smallicon'));
+        $cicon = html_writer::empty_tag('img', array('src' => $OUTPUT->pix_url('i/course'),
+                                              'class' => 'icon'));
+
+        foreach($mycourses as $k => $r) {
+            $list = array();
+            foreach($r->courses as $course) {
+                $link = new moodle_url('/course/view.php', array('id' => $course->id));
+
+                if($k === 'recommended') {
+                    $coursecontext = get_context_instance(CONTEXT_COURSE, $course->id);
+                    $coursecontactroles = explode(',', $CFG->coursecontact);
+
+                    $teachers = array();
+                    foreach ($coursecontactroles as $roleid) {
+                        $users = get_role_users($roleid, $coursecontext);
+                        foreach ($users as $user) {
+                            $teachers[] = $uicon . html_writer::link(new moodle_url($CFG->wwwroot.'/message/', 
+                                                            array('id' => $user->id)), fullname($user));
+                        }
+                    }
+                    $teachers = html_writer::alist($teachers);
+                }
+
+                $list[] = $cicon . html_writer::link($link, format_string($course->fullname)) . $teachers;
+            }
+            $this->content->text .= html_writer::tag('div', 
+                                                     html_writer::tag('h3', $r->enrolledas) .
+                                                     html_writer::alist($list, 
+                                                                      array('class' => 'unlist')),
+                                                     array('class' => $k));
+        }
+
+        $this->content->footer = html_writer::link(new moodle_url('/course/index.php'), 
+                                                   get_string("fulllistofcourses")) . " &hellip;";
 
         return $this->content;
     }
